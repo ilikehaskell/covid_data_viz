ba1390177ddd255239f9e30d7c4e3e342d2adba5
Șmit-Andrei Oanea <smitoanea@gmail.com>
2022-05-16 21:51:49 +0300

    Attempt to create hospitalization graph

--- src/widgets/new_widget.py
+++ src/widgets/new_widget.py
@@ -1,21 +1,31 @@
 import streamlit as st
 import pandas as pd
 
-from ..data_store import notification_rate_df
+from ..data_store import hospital_admission_df
 
-def new_widget(key = 0):
-    countries = sorted(list(set(notification_rate_df.country)))
+plot_type_dict = {'bar':st.bar_chart, 'area': st.area_chart, 'line':st.line_chart}
+
+def hospital_admission_widget(key = 0):
+    plot_type = st.sidebar.selectbox('Plot type', ['bar', 'area', 'line'])
+    
+
+    countries = sorted(list(set(hospital_admission_df.country)))
     romania_index = countries.index('Romania')
     country = st.selectbox('Country', options=countries, index=romania_index, key=key)
 
-    country_df = notification_rate_df[notification_rate_df.country == country]
+    country_df = hospital_admission_df[hospital_admission_df.country == country]
+    country_df = country_df.swapaxes(0,1)
     country_df = country_df.rename(columns={'year_week': 'YW'})
     country_df = country_df.set_index('YW')
-    country_df
-    st.header("Cases per 100.000, Deaths per 1 mil.")
-    cd_df = pd.concat( [country_df[country_df.indicator == 'cases'].rate_14_day.rename('cases'), country_df[country_df.indicator == 'deaths'].rate_14_day.rename('deaths')], axis=1)
-    st.line_chart(cd_df)
-    st.header("Deaths/Cases")
-    st.line_chart(cd_df.deaths/cd_df.cases/10)
+    #st.header("")
+    cd_df = pd.concat( [country_df[country_df.indicator == 'value'].rate_14_day.rename('value'), country_df[country_df.indicator == 'deaths'].rate_14_day.rename('deaths')], axis=1)
+
+
+
+    #st.line_chart(cd_df)
+    st.header("Hospital admissions")
+    plot_type_dict[plot_type](country_df, )
+   
+   #st.line_chart(country_df.head())
 
     # st.line_chart()
--- src/widgets/variants_widget.py
+++ src/widgets/variants_widget.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import pandas as pd
 
+#import iplot
+import cufflinks as cf
+
+cf.go_offline()
+cf.set_config_file(offline=False, world_readable=True)
+
 from ..data_store import variants_df
 
 plot_type_dict = {'bar':st.bar_chart, 'area': st.area_chart, 'line':st.line_chart}
 
 def variants_widget(key=0):
     
-    plot_type = st.sidebar.selectbox('Plot type', ['bar', 'area', 'line'])
+    plot_type = st.sidebar.selectbox('Plot typeg', ['bar', 'area', 'line'])
 
     st.write(variants_df.head())
     country_df = variants_df[variants_df.country == "Romania"]
-    #df_variants = variants_df['variant'].value_counts().rename_axis('unique_values').reset_index(name='counts')
+
+    #series = variants_df['variant'].value_counts().sort_values(ascending=False).to_frame()
+    #df_variants_plot = series.iplot('''kind='bar''', yTitle="Number of samples", title="Variants")
+     
 
 
-
-    #plot_type_dict[plot_type](df_variants)
+    #plot_type_dict[plot_type](df_variants_plot)
